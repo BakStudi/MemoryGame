77a466fce4edd826543c524c1099e54ada516153
BakStudi <drgkill@hotmail.com>
2025-09-16 02:09:57 +0200

    Création des 16 cartes en JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Fonction de création d'une carte
 function createCard(CardUrl) {
     const card = document.createElement('div');
     card.classList.add('card');
@@ -6,10 +7,39 @@ function createCard(CardUrl) {
     const cardContent = document.createElement('img');
     cardContent.classList.add('card-content');
     cardContent.src = `${CardUrl}`;
+
     card.appendChild(cardContent);
+    
     return card;
 }
 
 // Code pour tester la fonction -->
+/* const gameBoard = document.getElementById('game-board');
+gameBoard.appendChild(createCard('https://picsum.photos/id/243/100/100')); */
+
+const cards = [
+    'https://picsum.photos/id/237/100/100',
+    'https://picsum.photos/id/238/100/100',
+    'https://picsum.photos/id/239/100/100',
+    'https://picsum.photos/id/240/100/100',
+    'https://picsum.photos/id/241/100/100',
+    'https://picsum.photos/id/242/100/100',
+    'https://picsum.photos/id/243/100/100',
+    'https://picsum.photos/id/244/100/100'
+];
+
 const gameBoard = document.getElementById('game-board');
-gameBoard.appendChild(createCard('https://picsum.photos/id/243/100/100'));
+
+function duplicateArray (arraySimple) {
+    let arrayDouble = [];
+    arrayDouble.push(...arraySimple);
+    arrayDouble.push(...arraySimple);
+    return arrayDouble;
+}
+
+const allCards = duplicateArray(cards);
+
+allCards.forEach(card => {
+    const cardHtml = createCard(card);
+    gameBoard.appendChild(cardHtml);
+})
